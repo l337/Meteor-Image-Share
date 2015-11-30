eec4b97997eb93eeb376a5ba5624a6a252cab220
1337 <autonomousmachine@gmail.com>
2015-11-30 12:56:40 -0800

    using iron router to dynamically render templates

--- client/client_test.js
+++ client/client_test.js
@@ -1,37 +1,3 @@
-  
-  // routing
-  Router.configure({
-    layoutTemplate: 'ApplicationLayout'
-  });
-
-  Router.route('/', function () {
-    this.render('welcome', {
-      to:"main"
-    });
-  });
-
-  Router.route('/images', function () {
-    this.render('navbar', {
-      to:"navbar"
-    });
-    this.render('images', {
-      to:"main"
-    });
-  });
-
-  Router.route('/image/:_id', function () {
-    this.render('navbar', {
-      to:"navbar"
-    });
-    this.render('image', {
-      to:"main",
-      data:function() {
-        return Images.findOne({_id:this.params._id});
-      }
-    });
-  });
-
-  // infinite scroll
   Session.set('imageLimit',8);
   var lastScrollTop = 0;
 
@@ -49,12 +15,10 @@
     }
   });
 
-  // accounts config
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
   
-  // helpers
   Template.images.helpers({
     images:function() {
       if(Session.get('userFilter')) {
